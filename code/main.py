55d3c57a7d5bfedeb2f980d83fc05e98128b59a9
gianluca <gianluca.iavicoli04@gmail.com>
2023-04-27 00:17:06 +0200

    auto

--- code/main.py
+++ code/main.py
@@ -1,18 +1,19 @@
 import subprocess
 
 #######################
-series = "????"
+series = "Epic stories"
 part = 1
 outro = "Follow us for more"
 outfile = f"text_{part}.mp3"
 path = f"C:\\Users\\Mario\\Desktop\\Tiktok\\ChatGPT-TikTok\\audio{outfile}"
+
 
 #######################
 
 with open('script.txt', 'r') as file:
     text = file.read().rstrip().replace('"',"'")
 
-final_text = f"{series}. Part {part}.\n{text}\n{outro}"
+final_text = f"{series} part {part}.\n{text}\n{outro}"
 
 with subprocess.Popen(f'edge-tts --voice en-US-ChristopherNeural --text "{final_text}" --write-media "{path}"', stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
     pass
